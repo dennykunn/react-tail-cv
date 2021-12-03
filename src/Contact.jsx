8c7b2704a5dc56dcc8237dddde0d526f2fa0fde8
dennykunn <dennykunn@gmail.com>
2021-12-03 21:33:22 +0700

    11

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -6,12 +6,12 @@ export default class Contact extends Component {
   render() {
     return (
       <div style={{ background: '#3a4046' }}>
-        <div className="max-w-3xl md:grid grid-cols-7 mx-auto gap-12 md:px-0 px-4 md:py-0 py-2">
+        <div className="max-w-3xl md:grid grid-cols-7 mx-auto gap-12 md:px-0 px-4 py-16">
           <div className="col-span-3 relative">
             <img className="absolute md:inline-block hidden -top-20" src={ImageOne} alt="" />
           </div>
           <div className="col-span-4 space-y-10">
-            <div className="flex items-center space-x-2 text-white font-semibold text-xs mt-14">
+            <div className="flex items-center space-x-2 text-white font-semibold text-xs">
               <img className="w-6" src={IconOne} alt="" />
               <p>CONTACT</p>
             </div>
@@ -19,7 +19,7 @@ export default class Contact extends Component {
               <span className="text-white">Contact me</span>, feel free to call to write anytime
             </p>
           </div>
-          <div className="col-span-3 space-y-6 pb-16">
+          <div className="col-span-3 space-y-6">
             <div className="text-xs leading-5">
               <p className="text-gray-500">MY PHONE</p>
               <p className="text-gray-200">+62 882 7123 0905</p>
@@ -62,7 +62,7 @@ export default class Contact extends Component {
               {/* <input className="bg-transparent border-b border-gray-600 w-full py-4 text-xs outline-none text-white" type="text" placeholder="FULL NAME" /> */}
               <textarea className="bg-transparent border-b border-gray-600 w-full py-4 text-xs outline-none text-white" placeholder="MESSAGE" rows="4"></textarea>
             </div>
-            <div className="col-start-2 col-span-1 md:mt-0 mt-10 md:mb-0 mb-16">
+            <div className="col-start-2 col-span-1 md:mt-0 mt-14 md:mb-0">
               <button className="w-full py-3 text-white text-sm" style={{ backgroundColor: '#5bac4e' }} href="/">
                 SEND MESSAGE
               </button>
